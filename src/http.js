b58d193ee709750b7f0d104d581a41ea2a54eb3f
TheKitoInc <11139894+TheKito@users.noreply.github.com>
2024-06-21 19:31:12 -0300

    Create http.js

---
+++ src/http.js
@@ -0,0 +1,16 @@
+// http.js
+
+module.exports.makeServiceRequest = async function (connectionID, serviceURL, requestText) {
+  return await fetch(serviceURL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/octet-stream",
+      "X-Connection-Id": connectionID,
+    },
+    body: requestText,
+  })
+    .then((response) => response.arrayBuffer())
+    .then((responseArrayBuffer) => {
+      return Buffer.from(responseArrayBuffer);
+    });
+};
